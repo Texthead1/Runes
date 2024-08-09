daa02e7747cda04229a7b58efa7cb9095ea987c2
NefariousTechSupport <Jaskaran7852@gmail.com>
2024-08-09 14:35:59 +0100

    Refactor PortalTag

--- include/PortalTag.hpp
+++ include/PortalTag.hpp
@@ -1,4 +1,13 @@
-#pragma once
+/*
+	File:
+		PortalTag.hpp
+
+	Description:
+		Data structures for the figures and a class for interfacing with figures.
+*/
+
+#ifndef RUNES_PORTAL_TAG_H
+#define RUNES_PORTAL_TAG_H
 
 #include <cstdint>
 
@@ -17,21 +26,35 @@
 
 namespace Runes
 {
+
+// Disable alignment, these structs are meant to correspond to the figure data directly.
 #pragma pack(push, 1)
-	//Size should be 0x20
+
+	// Blocks 0x00 and 0x01 of the tag. Size must be 0x20
 	struct PortalTagHeader
 	{
-		/* 0x00 */ uint32_t _serial;
+		/* 0x00 */ uint32_t _serial;                 // Unique identifier for the figure.
+
 		/* 0x04 */ uint8_t _bcc;
 		/* 0x05 */ uint8_t _manufacturerData[0x0B];
-		/* 0x10 */ kTfbSpyroTag_ToyType _toyType;
-		/* 0x12 */ uint8_t _toyType_high;
-		/* 0x13 */ uint8_t _importantByte;		//MUST BE SET TO 0 OR THE GAME WILL REFUSE TO READ THIS TOY
-		/* 0x14 */ uint32_t _tradingCardId1;
-		/* 0x18 */ uint32_t _tradingCardId2;
-		/* 0x1C */ uint16_t _subType;
-		/* 0x1E */ uint16_t _headerChecksum;
+
+		/* 0x10 */ kTfbSpyroTag_ToyType _toyType;    // Character ID of the figure
+		/* 0x12 */ uint8_t _toyType_high;            // Character ID is actually 24 bits, this is never
+		                                             //  used despite being read
+
+		/* 0x13 */ uint8_t _importantByte;           // Not sure what this is, must be set to 0 or
+		                                             //  SuperChargers (maybe others) will refuse to read it.
+
+		/* 0x14 */ uint64_t _tradingCardId;          // Encoded webcode, 10 digit base 29 integer. The alphabet for it goes
+		                                             //  as follows: 23456789BCDFGHJKLMNPQRSTVWXYZ
+
+		/* 0x1C */ uint16_t _subType;                // Variant ID
+
+		/* 0x1E */ uint16_t _headerChecksum;         // See SkylanderFormat.md
 	};
+
+
+	// Stores a date and time. Size must be 0x05
 	struct PortalTagTimeOfDay
 	{
 		/* 0x00 */ uint8_t _minute;
@@ -40,60 +63,64 @@ namespace Runes
 		/* 0x03 */ uint8_t _month;
 		/* 0x04 */ uint16_t _year;
 	};
+
 	//Size should be 0xB0
 	struct PortalTagData
 	{
-		/* 0x00 */ uint16_t _experience2011_low;
-		/* 0x02 */ uint8_t _experience2011_high;
-		/* 0x03 */ uint16_t _coinCount;
-		/* 0x05 */ uint32_t _cumulativeTime;
-		/* 0x09 */ uint8_t _areaSequence0;
-		/* 0x0A */ uint16_t _crcType3;
-		/* 0x0C */ uint16_t _crcType2;
-		/* 0x0E */ uint16_t _crcType1;
+		/* 0x00 */ uint16_t _experience2011_low;     // This is just the Spyro's Adventure experience, caps at 33000.
+		/* 0x02 */ uint8_t _experience2011_high;     // Experience is a 24 bit int for some reason.
 
-		/* 0x10 */ uint16_t _flags1_low;
-		/* 0x12 */ uint8_t _flags1_high;
-		/* 0x13 */ uint8_t _platformUse2011;
-		/* 0x14 */ uint16_t _hat2011;
+		/* 0x03 */ uint16_t _coinCount;              // Money value for character, caps at 65535.
+
+		/* 0x05 */ uint32_t _cumulativeTime;         // Total time on portal in seconds.
+
+		/* 0x09 */ uint8_t _areaSequence0;           // See SkylanderFormat.md
+		/* 0x0A */ uint16_t _crcType3;               // See SkylanderFormat.md 
+		/* 0x0C */ uint16_t _crcType2;               // See SkylanderFormat.md
+		/* 0x0E */ uint16_t _crcType1;               // See SkylanderFormat.md
+
+		/* 0x10 */ uint16_t _flags1_low;             // See SkylanderFormat.md
+		/* 0x12 */ uint8_t _flags1_high;             // See SkylanderFormat.md
+		/* 0x13 */ uint8_t _platformUse2011;         // Platforms this figure has touched
+		/* 0x14 */ uint16_t _hat2011;                // Current hat (only ssa hats)
 		/* 0x16 */ uint8_t _regionCountCoded;
-		/* 0x17 */ uint8_t _platformUse2013;
-		/* 0x18 */ uint8_t unk18[8];			//Maybe an owner id?
+		/* 0x17 */ uint8_t _platformUse2013;         // Platforms this figure has touched
+		/* 0x18 */ uint8_t unk18[8];                 // Maybe an owner id?
 
 		/* 0x20 */ uint16_t _nickname[16];
 
-		/* 0x40 */ PortalTagTimeOfDay _recentlyUsed;
-		/* 0x46 */ uint32_t _heroics2011;
-		/* 0x4A */ uint16_t _heroPoints;
+		/* 0x40 */ PortalTagTimeOfDay _recentlyUsed; // The last time they were written to
+		/* 0x46 */ uint32_t _heroics2011;            // Bitfield of heroics
+		/* 0x4A */ uint16_t _heroPoints;             // Hero points, from skylanders universe, rest in peace
 		/* 0x4C */ uint8_t unk4C[3];
 		/* 0x4F */ uint8_t _ownerCount;
 
-		/* 0x50 */ PortalTagTimeOfDay _firstUsed;
+		/* 0x50 */ PortalTagTimeOfDay _firstUsed;    // Last time they were used after being reset
 		/* 0x56 */ uint8_t unk56[10];
 
 		/* 0x60 */ uint8_t unk60[12];
 		/* 0x6C */ uint32_t _challengeLevel;
 
-		/* 0x70 */ uint16_t _crcType6;
-		/* 0x72 */ uint8_t _areaSequence1;
-		/* 0x73 */ uint16_t _experience2012;
-		/* 0x75 */ uint8_t _hat2012;
-		/* 0x76 */ uint16_t _flags2;
-		/* 0x78 */ uint32_t _experience2013;
-		/* 0x7C */ uint8_t _hat2013;
+		/* 0x70 */ uint16_t _crcType6;               // See SkylanderFormat.md
+		/* 0x72 */ uint8_t _areaSequence1;           // See SkylanderFormat.md
+		/* 0x73 */ uint16_t _experience2012;         // Experience from giants, caps at either 63500 or 65535 depending on the character
+		/* 0x75 */ uint8_t _hat2012;                 // Current hat (only giants hats)
+		/* 0x76 */ uint16_t _flags2;                 // See SkylanderFormat.md
+		/* 0x78 */ uint32_t _experience2013;         // Experience for ssf onwards, caps at 101000
+		/* 0x7C */ uint8_t _hat2013;                 // Current hat (only swap force or trap team hats)
 		/* 0x7D */ uint8_t unk7D;
-		/* 0x7E */ uint8_t _hat2015;
+		/* 0x7E */ uint8_t _hat2015;                 // Current hat (superchargers hats)
 		/* 0x7F */ uint8_t unk7F;
 
-		/* 0x80 */ uint32_t _battlegroundsFlags;
-		/* 0x84 */ uint16_t _heroics2012_low;
-		/* 0x86 */ uint8_t _heroics2012_high;
-		/* 0x87 */ uint64_t _sgQuestsLow;
-		/* 0x8F */ uint8_t _sgQuestsHigh;
+		/* 0x80 */ uint32_t _battlegroundsFlags;     // flags exclusive to battlegrounds
+		/* 0x84 */ uint16_t _heroics2012_low;        // giants heroic challenges
+		/* 0x86 */ uint8_t _heroics2012_high;        // giants heroic challenges
+		/* 0x87 */ uint64_t _sgQuestsLow;            // giants quests
+		/* 0x8F */ uint8_t _sgQuestsHigh;            // giants quests
 
 		/* 0x90 */ uint8_t unk90[7];
-		/* 0x97 */ uint64_t _ssfQuestsLow;	//Also ssc driver quests
-		/* 0x9F */ uint8_t _ssfQuestsHigh;
+		/* 0x97 */ uint64_t _ssfQuestsLow;	          // Swap force quests, also ssc driver quests
+		/* 0x9F */ uint8_t _ssfQuestsHigh;            // Swap force quests, also ssc driver quests
 
 		/* 0xA0 */ uint8_t unkA0[16];
 
@@ -118,7 +145,12 @@ namespace Runes
 		//PortalTagTimeOfDay getFirstTime();
 		//void setFirstTime(PortalTagTimeOfDay firstTime);
 	};
+
 #pragma pack(pop)
+
+
+	// This class serves as an abstraction, allowing one to edit various parts of the figures
+	// without needing to deal without needing to deal with the way it was coded and such.
 	class PortalTag
 	{
 		public:
@@ -164,6 +196,7 @@ namespace Runes
 			void DebugPrintHeader();
 			void DebugSaveTagData();
 			bool isTrap();
+
 		private:
 			bool _tagHeaderStored;
 			bool _tagDataStored;
@@ -177,58 +210,6 @@ namespace Runes
 	};
 }
 
-#define assert_offset(type, offset, field) static_assert(offsetof(type, field) == offset, #field " has invalid offset")
-#define assert_size(type, size) static_assert(sizeof(type) == size, #type " has invalid size")
+#include "PortalTagAsserts.inc"
 
-assert_size(Runes::PortalTagHeader, 0x20);
-assert_offset(Runes::PortalTagHeader, 0x00, _serial);
-assert_offset(Runes::PortalTagHeader, 0x04, _bcc);
-assert_offset(Runes::PortalTagHeader, 0x10, _toyType);
-assert_offset(Runes::PortalTagHeader, 0x13, _importantByte);
-assert_offset(Runes::PortalTagHeader, 0x14, _tradingCardId1);
-assert_offset(Runes::PortalTagHeader, 0x18, _tradingCardId2);
-assert_offset(Runes::PortalTagHeader, 0x1C, _subType);
-assert_offset(Runes::PortalTagHeader, 0x1E, _headerChecksum);
-
-assert_size(Runes::PortalTagTimeOfDay, 0x06);
-assert_offset(Runes::PortalTagTimeOfDay, 0x00, _minute);
-assert_offset(Runes::PortalTagTimeOfDay, 0x01, _hour);
-assert_offset(Runes::PortalTagTimeOfDay, 0x02, _day);
-assert_offset(Runes::PortalTagTimeOfDay, 0x03, _month);
-assert_offset(Runes::PortalTagTimeOfDay, 0x04, _year);
-
-assert_size(Runes::PortalTagData, 0xB0);
-assert_offset(Runes::PortalTagData, 0x00, _experience2011_low);
-assert_offset(Runes::PortalTagData, 0x02, _experience2011_high);
-assert_offset(Runes::PortalTagData, 0x03, _coinCount);
-assert_offset(Runes::PortalTagData, 0x05, _cumulativeTime);
-assert_offset(Runes::PortalTagData, 0x09, _areaSequence0);
-assert_offset(Runes::PortalTagData, 0x0A, _crcType3);
-assert_offset(Runes::PortalTagData, 0x0C, _crcType2);
-assert_offset(Runes::PortalTagData, 0x0E, _crcType1);
-assert_offset(Runes::PortalTagData, 0x10, _flags1_low);
-assert_offset(Runes::PortalTagData, 0x12, _flags1_high);
-assert_offset(Runes::PortalTagData, 0x13, _platformUse2011);
-assert_offset(Runes::PortalTagData, 0x14, _hat2011);
-assert_offset(Runes::PortalTagData, 0x17, _platformUse2013);
-assert_offset(Runes::PortalTagData, 0x20, _nickname);
-assert_offset(Runes::PortalTagData, 0x40, _recentlyUsed);
-assert_offset(Runes::PortalTagData, 0x46, _heroics2011);
-assert_offset(Runes::PortalTagData, 0x4A, _heroPoints);
-assert_offset(Runes::PortalTagData, 0x4F, _ownerCount);
-assert_offset(Runes::PortalTagData, 0x50, _firstUsed);
-assert_offset(Runes::PortalTagData, 0x70, _crcType6);
-assert_offset(Runes::PortalTagData, 0x72, _areaSequence1);
-assert_offset(Runes::PortalTagData, 0x73, _experience2012);
-assert_offset(Runes::PortalTagData, 0x75, _hat2012);
-assert_offset(Runes::PortalTagData, 0x76, _flags2);
-assert_offset(Runes::PortalTagData, 0x78, _experience2013);
-assert_offset(Runes::PortalTagData, 0x7C, _hat2013);
-assert_offset(Runes::PortalTagData, 0x7E, _hat2015);
-assert_offset(Runes::PortalTagData, 0x80, _battlegroundsFlags);
-assert_offset(Runes::PortalTagData, 0x84, _heroics2012_low);
-assert_offset(Runes::PortalTagData, 0x86, _heroics2012_high);
-assert_offset(Runes::PortalTagData, 0x87, _sgQuestsLow);
-assert_offset(Runes::PortalTagData, 0x8F, _sgQuestsHigh);
-assert_offset(Runes::PortalTagData, 0x97, _ssfQuestsLow);
-assert_offset(Runes::PortalTagData, 0x9F, _ssfQuestsHigh);
+#endif // RUNES_PORTAL_TAG_H
